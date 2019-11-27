2b2fa1f3e57ba7225ec7bc96b43c8c78d8013dc7
GoBeromsu <310o@naver.com>
2019-11-27 13:50:12 +0900

    hi

--- src/main/java/com/GameMaster.java
+++ src/main/java/com/GameMaster.java
@@ -32,7 +32,7 @@ public class GameMaster {
     }
 
     private static void initiatePlayers() {
-        for(int i = 0; i<2; i++){
+        for (int i = 0; i < 2; i++) {
             players[i] = Player
                     .builder()
                     .pos(TileType.A)
@@ -43,27 +43,40 @@ public class GameMaster {
                     .build();
         }
     }
-    public static void initiateGame(){
+
+    public static void initiateGame() {
         token = 0;
         turn = 0;
         initiatePlayers();
         initMap();
         initMonsters();
     }
-    private static void initItem(){
+
+    private static void initItem() {
 //        Player.items
     }
-    private static void initMap(){}
-    private static void initMonsters(){}
-    public static void setBoss(Boss selected_boss){
+
+    private static void initMap() {
+    }
+
+    private static void initMonsters() {
+    }
+
+    public static void setBoss(Boss selected_boss) {
         // 테스트 코드 TODO : 실제 보스는 객체로 적절한 스탯으로 생성해야 함. enum이 될 순 없다. health가 변경가능해야 하므로
         current_boss = selected_boss;
     }
+
     public static boolean hasItem(ItemType item) {
-        return getCurrentPlayer().getItems().indexOf(item)==-1? true:false;
+        return getCurrentPlayer().getItems().indexOf(item) == -1 ? true : false;
     }
-    public static boolean checkEnergy(Player player){return true;}
-    public static void setInitializePlayerStats(int[] combined_stats){
+// 플레이어의 현재 에너지가 0인지 아닌지 체크
+    // 0이면 false 아니면 true
+    public static boolean checkEnergy(Player player) {
+        return getCurrentPlayer().getEnergy() != 0 ? true:false;
+    }
+
+    public static void setInitializePlayerStats(int[] combined_stats) {
         Player player = getCurrentPlayer();
         player.setPower(combined_stats[0]);
         player.setDexterity(combined_stats[1]);
@@ -72,20 +85,43 @@ public class GameMaster {
         // 체력만 기존 체력 + SetStatus_page에서 추가한 포인트로 세팅한다.
         player.setHealth(player.getHealth() + combined_stats[4]);
 
-        if(player.getHealth() > 3) // player의 체력이 3 이상이면 여러번 움직일 수 있다.
+        if (player.getHealth() > 3) // player의 체력이 3 이상이면 여러번 움직일 수 있다.
             player.setEnergy(player.getHealth() / 3);
-        else if(player.getHealth() > 0) // player가 죽지 않았으면 1번 이상 움직일 수 있다.
+        else if (player.getHealth() > 0) // player가 죽지 않았으면 1번 이상 움직일 수 있다.
             player.setEnergy(1); // 최소 값
         else // 플레이어가 죽었으면 움직일 수 없다.
             player.setEnergy(0);
     }
-    public static void death(Player player){}
-    private static void revive(Player player){}
-    private static int turnEnd(){return 0;}
-    private static boolean check_num_of_token_for_win(){return true;}
-    private static boolean check_player_status_for_lost(){return true;}
-    private static boolean check_num_of_monsters_portals_for_boss(){return true;}
-    public static String[] getGeneralDialogues(){return new String[0];}
-    public static void setPortalGateRandomly(){}
-    public static void generateBossFight(Boss boss){}
+
+    public static void death(Player player) {
+    }
+
+    private static void revive(Player player) {
+    }
+
+    private static int turnEnd() {
+        return 0;
+    }
+
+    private static boolean check_num_of_token_for_win() {
+        return true;
+    }
+
+    private static boolean check_player_status_for_lost() {
+        return true;
+    }
+
+    private static boolean check_num_of_monsters_portals_for_boss() {
+        return true;
+    }
+
+    public static String[] getGeneralDialogues() {
+        return new String[0];
+    }
+
+    public static void setPortalGateRandomly() {
+    }
+
+    public static void generateBossFight(Boss boss) {
+    }
 }
--- src/main/java/com/ItemType.java
+++ src/main/java/com/ItemType.java
@@ -10,7 +10,6 @@ import lombok.Data;
  * @version 1.0
  */
 public enum ItemType {
-    EXCALIBUR("엑스칼리버", 2, 1, 0, 1, 0, 5),
     NECRONOMICON("네크로노미", 0, 0, 4, 0, -1, 5),
     SHININGANISOTROPICAMY("빛나는 부등변다면체", 1, 1, 1, 1, 1, 5),
     GOLDENHONEYLIQUOR("황금의 벌꿀술", 2, 0, 0, 2, 0, 5),
@@ -26,19 +25,7 @@ public enum ItemType {
     HANZOSWORD("핫토리한조의 검",2,2,0,0,0,5),
     M16("M16",3,0,0,0,0,3);
     private int p_power;
-    private int p_dex;
-    private int p_int;
-    private int p_health;
-    private int p_mental;
-    private String name;
-    private int price;
-
-    ItemType(String name, int p_power, int p_dex, int p_int, int p_health, int p_mental, int price) {
-        this.name = name;
-        this.p_power = p_power;
-        this.p_dex = p_dex;
         this.p_int = p_int;
-        this.p_health = p_health;
         this.p_mental = p_mental;
         this.price = price;
     }
@@ -46,27 +33,21 @@ public enum ItemType {
     public String getName() {
         return name;
     }
-
     public int getPower() {
         return p_power;
     }
-
     public int getDex() {
         return p_dex;
     }
-
     public int getInt() {
         return p_int;
     }
-
     public int getHealth() {
         return p_health;
     }
-
     public int getMental() {
         return p_mental;
     }
-
     public int getPrice() {
         return price;
     }
